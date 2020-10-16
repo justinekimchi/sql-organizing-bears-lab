c69d68d049a4b45b49dd3d06348f8a01ee07cbf6
justinekimchi <justine.kim.manaloto@gmail.com>
2020-10-16 11:06:01 -0500

    done with bears

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,15 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color)
-  FROM bears
-  WHERE color = 'dark brown'"
+  # "SELECT color, COUNT(color)
+  # FROM bears
+  # WHERE color = 'dark brown'"
 
-  #"SELECT color, COUNT(color) FROM bears GROUP BY color LIMIT 1"
+  "SELECT color, COUNT(color) AS count
+  FROM bears 
+  GROUP BY color
+  ORDER BY count DESC 
+  LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -33,5 +37,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE id = 8"
+  "SELECT * FROM bears WHERE temperament = 'aggressive'"
 end
